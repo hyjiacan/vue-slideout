ab44306d411952d823caecb730bd274566b02f72
hyjiacan <hyjiacan@163.com>
2019-10-14 11:33:32 +0800

    Modified   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   publicPath: './',
   productionSourceMap: false,
-  chainWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // 清除css，js版本号
-      config.output.filename('js/[name].js').end();
-      config.output.chunkFilename('js/[name].js').end();
-      // 为生产环境修改配置...
-      config.plugin('extract-css').tap(args => [{
-        filename: `css/[name].css`,
-        chunkFilename: `css/[name].css`
-      }])
-    }
-  }
+  filenameHashing: false
 }
