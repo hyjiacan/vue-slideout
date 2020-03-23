aa8c23ae41034bf0130694cdab54f8ab028056b8
hyjiacan <hyjiacan@163.com>
2020-03-23 16:28:15 +0800

    FIX issue on `render-when-visible`

--- src/components/Template.vue
+++ src/components/Template.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vue-slideout" :style="containerStyle" :class="containerClasses" tabindex="0">
     <div class="vue-slideout-mask" v-if="showMask" @click="onMaskClick" :style="maskStyle"></div>
-    <div class="vue-slideout-layout" v-if="!renderWhenVisible || visible" :style="layoutStyle" ref="layout">
+    <div class="vue-slideout-layout" v-if="!renderWhenVisible || isVisible" :style="layoutStyle" ref="layout">
       <div class="vue-slideout-drag-handle" v-if="allowResize && !isFullscreen && !isSizeFixed"
            @mousedown="mouseDownHandler"></div>
       <div class="vue-slideout-header" :style="headerStyle" v-if="showHeader">
