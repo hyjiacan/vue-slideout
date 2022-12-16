8de6b9f45b28357983fbf45fedb7a3aa406e97ed
hyjiacan <hyjiacan@163.com>
2022-12-16 16:29:52 +0800

    FIX #14 Do not work for single component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import Slideout from './slideout.js'
 
-function setDefaultProps (customize) {
+function setDefaultProps(customize) {
   for (let prop in customize) {
     if (!customize.hasOwnProperty(prop)) {
       continue
     }
     // Parse property name like "abc_xyz" into "abcXyz".
-    prop = prop.replace(/-[a-z]/g, match => {
+    prop = prop.replace(/_[a-z]/g, match => {
       return match[1].toUpperCase()
     })
     if (!Slideout.props.hasOwnProperty(prop)) {
@@ -21,11 +21,11 @@ function setDefaultProps (customize) {
  * @param app
  * @param {Object} defaults The defaults value
  */
-export default {
-  install (app, defaults) {
-    if (defaults) {
-      setDefaultProps(defaults)
-    }
-    app.component(Slideout.name, Slideout)
+Slideout.install = function (app, defaults) {
+  if (defaults) {
+    setDefaultProps(defaults)
   }
+  app.component(Slideout.name, Slideout)
 }
+
+export default Slideout
